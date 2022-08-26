383f4cd27afcb570c5ffefdfe30c8f23ec3ff92c
new ADILAYOUB <mintofree840@gmail.com>
2022-08-26 22:03:43 +0530

    folder Structuring

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:search/screens/home_page.dart';
+import 'package:search/screens/bubble/bubble.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/screens/search/search.dart
@@ -0,0 +1,121 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  _HomeScreenState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filteredNames = names;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
+
+  final TextEditingController _filter = TextEditingController();
+  final dio = Dio();
+  String _searchText = "";
+  List names = [];
+  List filteredNames = [];
+  Icon _searchIcon = const Icon(Icons.search);
+  Widget _appBarTitle = const Text('Search Example');
+
+  //step 2.1
+  void _getNames() async {
+    final response =
+        await dio.get('https://jsonplaceholder.typicode.com/users');
+    print(response.data);
+    List tempList = [];
+    for (int i = 0; i < response.data.length; i++) {
+      tempList.add(response.data[i]);
+    }
+    setState(() {
+      names = tempList;
+      filteredNames = names;
+    });
+  }
+
+//Step 2.2
+  void _searchPressed() {
+    setState(() {
+      if (_searchIcon.icon == Icons.search) {
+        _searchIcon = const Icon(Icons.close);
+        _appBarTitle = TextField(
+          controller: _filter,
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.search), hintText: 'Search...'),
+        );
+      } else {
+        _searchIcon = const Icon(Icons.search);
+        _appBarTitle = const Text('Search Example');
+        filteredNames = names;
+        _filter.clear();
+      }
+    });
+  }
+
+  //Step 4
+  Widget _buildList() {
+    if (_searchText.isNotEmpty) {
+      List tempList = [];
+      for (int i = 0; i < filteredNames.length; i++) {
+        if (filteredNames[i]['name']
+            .toLowerCase()
+            .contains(_searchText.toLowerCase())) {
+          tempList.add(filteredNames[i]);
+        }
+      }
+      filteredNames = tempList;
+    }
+    return ListView.builder(
+      itemCount: filteredNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(filteredNames[index]['name']),
+          onTap: () => print(filteredNames[index]['name']),
+        );
+      },
+    );
+  }
+
+  //STep6
+  _buildBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    _getNames();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body: Container(
+        child: _buildList(),
+      ),
+//
+    );
+  }
+}
